c55a7402a1fca019907e0528d12e8e62b40a976a
Jireh <tohjireh9@gmail.com>
2025-08-17 19:19:20 +0800

    added add and list features using fixed array

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 
 public class Nova {
     private static final String DIVIDER = "____________________________________________________________\n";
+    private static final String[] list = new String[100];
+    private static int listSize = 0;
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println(DIVIDER +
                 "Hello! I'm Nova :3\n" + "What can I do for you?\n" +
@@ -12,9 +15,20 @@ public class Nova {
 
         String line = scanner.nextLine();
         while (!line.equals("bye")) {
-            System.out.println(DIVIDER + line + "\n" + DIVIDER);
+            if (line.equals("list")) {
+                StringBuilder tasks = new StringBuilder();
+                for (int i = 1; i < listSize + 1; i++) {
+                    tasks.append(i).append(". ").append(list[i - 1]).append("\n");
+                }
+                System.out.println(DIVIDER + tasks + DIVIDER);
+                line = scanner.nextLine();
+            }
+            System.out.println(DIVIDER + "added: " + line + "\n" + DIVIDER);
+            list[listSize] = line;
+            listSize++;
             line = scanner.nextLine();
         }
+
         System.out.println(DIVIDER + "Bye. Hope to see you again soon!\n" + DIVIDER);
         return;
     }
