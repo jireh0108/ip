5cd8edc10999c8666e7c3068177d9704ac199e92
Jireh <tohjireh9@gmail.com>
2025-08-17 18:53:23 +0800

    added echo feature and exit on bye

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,10 +1,20 @@
 package main.java;
+import java.util.Scanner;
 
 public class Nova {
     public static void main(String[] args) {
-        String divider = "____________________________________________________________";
-        System.out.println(divider + "\n" +
-                "Hello! I'm Nova\n" + "What can I do for you?\n" + divider + "\n"
-                + "Bye. Hope to see you again soon!\n" + divider + "\n");
+        String divider = "____________________________________________________________\n";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(divider +
+                "Hello! I'm Nova :3\n" + "What can I do for you?\n" +
+                divider);
+
+        String line = scanner.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(divider + line + "\n" + divider);
+            line = scanner.nextLine();
+        }
+        System.out.println(divider + "Bye. Hope to see you again soon!\n" + divider);
+        return;
     }
 }
