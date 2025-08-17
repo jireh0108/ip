d12df075841b9bf03571e0af0eb31d64b707c440
Jireh <tohjireh9@gmail.com>
2025-08-17 18:58:53 +0800

    made divider static and final

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -2,19 +2,20 @@ package main.java;
 import java.util.Scanner;
 
 public class Nova {
+    private static final String DIVIDER = "____________________________________________________________\n";
+
     public static void main(String[] args) {
-        String divider = "____________________________________________________________\n";
         Scanner scanner = new Scanner(System.in);
-        System.out.println(divider +
+        System.out.println(DIVIDER +
                 "Hello! I'm Nova :3\n" + "What can I do for you?\n" +
-                divider);
+                DIVIDER);
 
         String line = scanner.nextLine();
         while (!line.equals("bye")) {
-            System.out.println(divider + line + "\n" + divider);
+            System.out.println(DIVIDER + line + "\n" + DIVIDER);
             line = scanner.nextLine();
         }
-        System.out.println(divider + "Bye. Hope to see you again soon!\n" + divider);
+        System.out.println(DIVIDER + "Bye. Hope to see you again soon!\n" + DIVIDER);
         return;
     }
 }
