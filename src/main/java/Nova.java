118e23010162a04f373ce0300d7393ba8215c5cd
Jireh <tohjireh9@gmail.com>
2025-08-17 18:42:52 +0800

    renamed chatbot to Nova, added simple greetings

---
+++ src/main/java/Nova.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Nova {
+    public static void main(String[] args) {
+        String divider = "____________________________________________________________";
+        System.out.println(divider + "\n" +
+                "Hello! I'm Nova\n" + "What can I do for you?\n" + divider + "\n"
+                + "Bye. Hope to see you again soon!\n" + divider + "\n");
+    }
+}
