0fadda41d8505d75402b8a7ddc904ad4081e0030
Jireh <tohjireh9@gmail.com>
2025-08-17 21:16:25 +0800

    created Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+package main.java;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void mark() {
+        this.isDone = !isDone;
+    }
+}
