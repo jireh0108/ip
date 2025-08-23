4cb6ab51c3460c6bea7b5385f1108249aa0044b3
Jireh <tohjireh9@gmail.com>
2025-08-24 04:15:18 +0800

    Implement date and time logic with schedule <date> command

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,25 +1,30 @@
 package main.java;
 
-public class Event extends Task {
-    protected String from;
-    protected String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+
+    public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
-    public String getFrom() {
+    public LocalDateTime getFrom() {
         return this.from;
     }
 
-    public String getTo() {
+    public LocalDateTime getTo() {
         return this.to;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: "
+                + from.format(DateTimeFormatter.ofPattern("MMM d yyyy HHmm")) + " to: "
+                + to.format(DateTimeFormatter.ofPattern("MMM d yyyy HHmm")) + ")";
     }
 }
